6a421bda27cc1c68426030de9a7461b9f1abba83
Sloane <sloanehawkins@gmail.com>
2020-01-08 11:28:10 -0800

    Add drill 7

---
+++ Drill7/Drill7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+            Console.WriteLine("While loop: ");
+            Console.ReadLine();
+            int a = 0;
+
+            while (a < 0) 
+            {
+                
+                Console.WriteLine("The value of a is {0}", a); //display the value of a 
+                a++; // add 1 to a 
+                Console.ReadLine();
+              
+                
+                
+            }
+
+            //do while loop
+            Console.WriteLine("Do while loop: ");
+            Console.ReadLine();
+            int b = 0;
+
+            // do loop will always execute at least once
+            do
+            {
+               
+                Console.WriteLine("The value of b is {0}", b); //display the value of b
+                b++; //add 1 to b 
+
+                Console.ReadLine();
+                
+            } while (b < 0);
+
+            Console.ReadLine();
+
+        }
+    }
+}
