857d39ce25c2c8ea8ba3b73d5996cc39c44e85e4
Sloane <sloanehawkins@gmail.com>
2020-01-14 13:54:16 -0800

    Add drill 11
    try/catch

---
+++ Drill11/Drill11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Create a list of integers. Ask the user for a number to divide each number in the list by. 
+            //Write a loop that takes each integer in the list, divides it by the number the user entered, 
+            //and displays the result to the screen.
+            List<int> numbers = new List<int>() { 10, 20, 30, 40, 50 };
+            Console.WriteLine("Enter a number to divide each number in the list by.");
+            
+            try
+            {
+                foreach (int number in numbers)
+                {
+                    int numInput = Convert.ToInt32(Console.ReadLine());
+                    int result = number / numInput;
+                    Console.WriteLine(number + " divided by " + numInput + " equals " + result);
+
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine("The program has emerged from the try/catch block.");
+            Console.ReadLine();
+        }
+    }
+}
