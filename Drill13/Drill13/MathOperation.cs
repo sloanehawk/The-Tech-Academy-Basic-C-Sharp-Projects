3ef6a252ff893a3aeda58987c19ba7c24cd0feed
Sloane <sloanehawkins@gmail.com>
2020-01-15 15:27:48 -0800

    Add drill 13
    Classes and methods

---
+++ Drill13/Drill13/MathOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill13
+{
+    public class MathOperation
+    {
+        //1. Create a class. In that class, create a method that will take in an integer,
+        //do a math operation to it and then return the answer as an integer.
+        public int Multiply(int num1)
+        {
+            int result = num1 * 2;
+            return result;
+        }
+
+        //3. Add a second method to the class, with the same name, that will take in a decimal,
+        //do a different math operation to it and then return the answer as an integer.
+        public int Multiply(decimal dec1)
+        {
+            decimal result = dec1 * 10;
+            int resultInt = Convert.ToInt32(result);
+            return resultInt;
+        }
+
+        //5. Add a third method to the class, with the same name, that will take in a string, 
+        //convert it to an integer if possible, do a different math operation to it 
+        //and then return the answer as an integer.
+        public int Multiply(string str1)
+        {
+            int result = Convert.ToInt32(str1);
+            result = result + 2;
+            return result;
+        }
+    }
+}
---
+++ Drill13/Drill13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method of the console app, 
+            //instantiate the class and call the one method, passing in an integer.
+            //Display the result to the screen.
+
+
+            MathOperation m = new MathOperation();
+            int a = 5;
+
+            int result1 = m.Multiply(a);
+            Console.WriteLine("Result 1: " + result1);
+
+            //In the Main() method of the console app, 
+            //instantiate the class and call the second method, passing in a decimal.
+            //Display the result to the screen.
+            decimal b = 4.5m;
+
+            int result2 = m.Multiply(b);
+            Console.WriteLine("Result 2: " + result2);
+
+            //6. In the Main() method of the console app, 
+            //instantiate the class and call the third method, 
+            //passing in a string that equates to an integer. Display the result to the screen.
+            string c = "7";
+
+            int result3 = m.Multiply(c);
+            Console.WriteLine("Result 3: " + result3);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
