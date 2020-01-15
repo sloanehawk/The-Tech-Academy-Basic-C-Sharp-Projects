f68afd28889aa0d9dbfdc77f5297a61ae3dff9bd
Sloane <sloanehawkins@gmail.com>
2020-01-15 11:35:11 -0800

    Add drill12
    classes and methods

---
+++ Drill12/Drill12/MathOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill12
+{
+    public class MathOperation
+    {
+        public int PlusTwo(int num1)
+        {
+            int result = num1 + 2;
+            return result;
+        }
+        public int TimesTwo(int num2)
+        {
+            int result = num2 * 2;
+            return result;
+        }
+        public int DividedByTwo(int num3)
+        {
+            int result = num3 / 2;
+            return result;
+        }
+    }
+}
---
+++ Drill12/Drill12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer.");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            MathOperation n = new MathOperation();
+
+            int result1 = n.PlusTwo(a);
+            Console.WriteLine("Your number plus two is " + result1);
+
+            int result2 = n.TimesTwo(a);
+            Console.WriteLine("Your number times two is " + result2);
+
+            int result3 = n.DividedByTwo(a);
+            Console.WriteLine("Your number divided by two is " + result3);
+            Console.ReadLine();
+
+        }
+    }
+}
