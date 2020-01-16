2307270a7957d45a7b87b97185a2516820e57ccf
Sloane <sloanehawkins@gmail.com>
2020-01-15 17:24:36 -0800

    Add drill 15
    Void method

---
+++ Drill15/Drill15/MathOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill15
+{
+    public class MathOperation
+    {
+        //1. Create a class. In that class, create a void method that takes two integers as parameters. 
+        //Have the method do a math operation on the first integer and display the second integer to the screen.
+        public void TimesTwo(int int1, int int2)
+        {
+            int result = int1 * 2;
+            Console.WriteLine(int2);
+
+        }
+
+
+    }
+}
---
+++ Drill15/Drill15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method of the console app, instantiate the class.
+            MathOperation operation = new MathOperation();
+
+            //3. Call the method in the class, passing in two numbers.
+            operation.TimesTwo(2, 3);
+
+
+            //4. Call the method in the class, specifying the parameters by name.
+            operation.TimesTwo(int1: 4, int2: 7);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
