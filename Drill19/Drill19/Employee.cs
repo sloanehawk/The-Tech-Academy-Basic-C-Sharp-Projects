f4b57d6c0f5a7a7290c4c0492532d042243155ff
Sloane <sloanehawkins@gmail.com>
2020-01-20 11:32:51 -0800

    Add drill 19
    Interfaces

---
+++ Drill19/Drill19/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill19
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name : " + FirstName + " " + LastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Quit method implemented");
+        }
+    }
+}
