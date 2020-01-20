d5beb0a9809f336fbca3654d841bb34046d880c8
Sloane <sloanehawkins@gmail.com>
2020-01-20 10:45:46 -0800

    Add drill 18
    abstract class

---
+++ Drill18/Drill18/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill18
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+       
+    }
+}
