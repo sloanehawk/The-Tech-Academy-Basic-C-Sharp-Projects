d89dbcbd7b2986783964f3fb6f46a3b95d6c59e0
Sloane <sloanehawkins@gmail.com>
2020-01-22 12:46:16 -0800

    Add drill 23
    struct

---
+++ Drill23/Drill23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 3.57m;
+            Console.WriteLine("Amount: " + num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
