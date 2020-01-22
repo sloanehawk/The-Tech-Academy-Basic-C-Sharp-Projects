0aa344f14ed79dce86ca927ce568a924830f010e
Sloane <sloanehawkins@gmail.com>
2020-01-22 11:15:51 -0800

    Add drill 21
    Generic type

---
+++ Drill21/Drill21/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill21
+{
+    public class Employee<T> : Person
+    {
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+
+    }
+}
---
+++ Drill21/Drill21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "a", "b", "c", "d" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>() { 5, 10, 15, 20 };
+
+            //create a loop that prints all of the Things to the console
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            
+            foreach(int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
