3961bc45d9afd5055907519f1c543bb96cf1250d
Sloane <sloanehawkins@gmail.com>
2020-01-21 18:01:25 -0800

    Add drill 20
    operator overloading

---
+++ Drill20/Drill20/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill20
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+
+            if (employee1.Id == employee2.Id)
+            {
+                bool employeesEqual = true;
+                return employeesEqual;
+            }
+            else
+            {
+                bool employeesEqual = false;
+                return employeesEqual;
+            }
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                bool employeesNotEqual = true;
+                return employeesNotEqual;
+            }
+            else
+            {
+                bool employeesNotEqual = false;
+                return employeesNotEqual;
+            }
+        }
+    }
+}
---
+++ Drill20/Drill20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            employee1.Id = 207;
+            employee2.Id = 208;
+
+            bool employeesEqual = employee1 == employee2;
+            Console.WriteLine("Employee 1 and Employee 2 have the same ID: " + employeesEqual);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
