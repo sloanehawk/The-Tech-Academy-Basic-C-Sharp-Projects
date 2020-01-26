fbb2b7342b8c2ade117d3bfd6437553379e67b86
Sloane <sloanehawkins@gmail.com>
2020-01-26 12:31:46 -0800

    Add drill 25
    File IO

---
+++ Drill25/Drill25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Drill25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\myProjects\numbers.txt", num);
+
+            string readText = File.ReadAllText(@"C:\Users\Student\myProjects\numbers.txt");
+            Console.WriteLine(readText);
+            Console.ReadLine();
+
+        }
+    }
+}
