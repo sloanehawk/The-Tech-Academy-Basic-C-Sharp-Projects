1462f6d31a483110b28d20dddb8eb0c2979e388d
Sloane <sloanehawkins@gmail.com>
2020-01-27 10:39:20 -0800

    Add drill 26
    DateTime

---
+++ Drill26/Drill26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Please enter a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            var then = now.AddHours(hours);
+            Console.WriteLine("In " + hours + " hours, it will be " + then);
+            Console.ReadLine();
+
+        }
+    }
+}
