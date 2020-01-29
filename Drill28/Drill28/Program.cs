acd67abd749d71c459a5084c799d8fe6390e0906
Sloane <sloanehawkins@gmail.com>
2020-01-29 10:21:08 -0800

    Add drill 28
    Exception handling

---
+++ Drill28/Drill28/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your age: ");
+            
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age == 0)
+                {
+                    throw new AgeException("You must enter a non zero integer.");
+                }
+                if (age<0)
+                {
+                    throw new AgeException("You must enter a positive integer.");
+                }
+                DateTime dateBorn = DateTime.Now.AddYears(-age); //Subtract the user's age from the current year
+                int yearBorn = dateBorn.Year;
+                Console.WriteLine("You were born in " + yearBorn);
+                
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You entered an invalid age.");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
