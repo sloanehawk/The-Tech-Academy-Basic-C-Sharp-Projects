5f9a5ca654a4e11e55d42ae4eee7a3446e980319
Sloane <sloanehawkins@gmail.com>
2020-01-29 09:06:08 -0800

    Add drill 27

---
+++ Drill27/Drill27/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill27
+{
+    public class Employee
+    {
+        //3. Chain two constructors together.
+        public Employee(): this(10)
+        {
+           
+        }
+
+        public Employee(int id)
+        {
+
+        }
+    }
+}
---
+++ Drill27/Drill27/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Create a const variable.
+            const string drill = "Drill 27";
+            //2. Create a variable using the keyword "var".
+            var course = "C # & .NET";
+
+        }
+    }
+}
